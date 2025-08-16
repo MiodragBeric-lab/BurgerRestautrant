769493e14a7e180cccc7bcd6121ecd128e2364fc
MiodragBeric-lab <miodragprgmng@gmail.com>
2025-08-16 18:39:43 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Item coke = new Item("drink", "coke",1.5);
+        coke.printItem();
+        coke.setSize("BIG");
+        coke.printItem();
+
+
+
+    }
+}
